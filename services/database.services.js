672efca9a57a90c110ca5b509eda2ada504984ae
HHUUYYLLEE <lebahuybk@gmail.com>
2024-05-09 06:26:46 +0700

    temp

--- services/database.services.js
+++ services/database.services.js
@@ -4,7 +4,7 @@ const { envConfig } = require("../constants/config");
 const connectDB = async () => {
   try {
     const options = {};
-    await mongoose.connect(envConfig.mongoURI || envConfig.mongoURL, options);
+    await mongoose.connect(envConfig.mongoURI, options);
     console.log("Connected to mongoDB");
   } catch (error) {
     console.log(error);
